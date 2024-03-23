bb2885781c7da2d46e293d2d0aa8490e5a3e6ae2
Hareesh Haridas <hareeshharidas2003@gmailcom>
2024-03-23 18:29:59 +0530

    done address handling in checkout screen

--- lib/application/presentation/screens/changeAddressScreen/change_address_screen.dart
+++ lib/application/presentation/screens/changeAddressScreen/change_address_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kicks_cart/Domain/models/address/get_address_model.dart';
 import 'package:kicks_cart/application/business%20logic/address/bloc/bloc/address_bloc.dart';
 import 'package:kicks_cart/application/presentation/utils/colors.dart';
 import 'package:kicks_cart/application/presentation/utils/constants.dart';
@@ -11,50 +12,138 @@ class ChangeAddressScreen extends StatefulWidget {
   State<ChangeAddressScreen> createState() => _ChangeAddressScreenState();
 }
 
+String selected = '2323';
+
 class _ChangeAddressScreenState extends State<ChangeAddressScreen> {
+  late AddressBloc addressBloc;
+  int? slectedAddress;
+  @override
+  void initState() {
+    super.initState();
+    addressBloc = context.read<AddressBloc>();
+    addressBloc.add(FetchAddressEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kWhite,
-      body: SafeArea(child: BlocBuilder<AddressBloc, AddressState>(
-        builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            kHeight38,
+            Row(
               children: [
-                kHeight38,
-                Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        icon: Icon(Icons.arrow_back)),
-                    Text(
-                      'Change Shipping Address',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                kHeight38,
-                Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: 10,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        height: 200,
-                        width: 300,
-                        decoration: BoxDecoration(border: Border.all()),
-                      );
+                IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
                     },
-                  ),
+                    icon: const Icon(Icons.arrow_back)),
+                const Text(
+                  'Change Shipping Address',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
-          );
-        },
+            kHeight38,
+            BlocBuilder<AddressBloc, AddressState>(
+              builder: (context, state) {
+                if (state is LoadingAddressState) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is LoadedAddressState) {
+                  List<GetAddressModel> address = state.address;
+                  return Expanded(
+                    child: ListView.separated(
+                      separatorBuilder: (context, index) => kHeight10,
+                      shrinkWrap: true,
+                      itemCount: address.length,
+                      itemBuilder: (context, index) {
+                        return Row(
+                          children: [
+                            Radio(
+                                value: index,
+                                groupValue: slectedAddress,
+                                onChanged: (int? value) {
+                                  setState(() {
+                                    slectedAddress = value;
+                                    selected = address[index].id;
+                                  });
+                                }),
+                            Container(
+                              height: 100,
+                              width: 200,
+                              decoration: BoxDecoration(border: Border.all()),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 12),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        kWidth30,
+                                        Text(address[index].name)
+                                      ],
+                                    ),
+                                    kHeight10,
+                                    Row(
+                                      children: [
+                                        kWidth30,
+                                        Text(address[index]
+                                            .phoneNumber
+                                            .toString())
+                                      ],
+                                    ),
+                                    kHeight10,
+                                    Row(
+                                      children: [
+                                        kWidth30,
+                                        Text(address[index].cityName)
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  );
+                } else if (state is ErrorAddressState) {
+                  return const Center(child: Text('Error Loading Address'));
+                } else {
+                  return const Center(child: Text('Unknown Error'));
+                }
+              },
+            ),
+            MaterialButton(
+              onPressed: () {
+                if (slectedAddress != null) {
+                  Navigator.of(context).pop(selected);
+                  context.read<AddressBloc>().add(FetchAddressEvent());
+                }
+              },
+              color: Colors.green,
+              textColor: kWhite,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              height: 50,
+              minWidth: 300,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Ship to this Address',
+                    style: TextStyle(color: kWhite, fontSize: 20),
+                  ),
+                  kHeight10
+                ],
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
--- lib/application/presentation/screens/checkoutScreen/checkout_screen.dart
+++ lib/application/presentation/screens/checkoutScreen/checkout_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kicks_cart/Domain/models/address/get_address_model.dart';
+import 'package:kicks_cart/application/business%20logic/address/bloc/bloc/address_bloc.dart';
 import 'package:kicks_cart/application/presentation/screens/changeAddressScreen/change_address_screen.dart';
 import 'package:kicks_cart/application/presentation/screens/checkoutScreen/widgets/product_detail_widget.dart';
 import 'package:kicks_cart/application/presentation/utils/colors.dart';
@@ -12,6 +15,22 @@ class CheckoutScreen extends StatefulWidget {
 }
 
 class _CheckoutScreenState extends State<CheckoutScreen> {
+  late AddressBloc addressBloc;
+  String selectedOne = '';
+  @override
+  void initState() {
+    super.initState();
+    addressBloc = context.read<AddressBloc>();
+    addressBloc.add(FetchAddressEvent());
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    addressBloc = context.read<AddressBloc>();
+    addressBloc.add(FetchAddressEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,134 +62,197 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
               kHeight30,
               const ProductDetailWidget(),
               kHeight30,
-              Container(
-                height: 550,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    border: Border.all(color: kGrey),
-                    borderRadius: BorderRadius.circular(13)),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 13),
-                  child: Column(
-                    children: [
-                      kHeight20,
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Subtotal',
-                            style: const TextStyle(fontSize: 17),
-                          ),
-                          Text('₹2000')
-                        ],
-                      ),
-                      kHeight38,
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Shipping Charge',
-                            style: TextStyle(fontSize: 17),
-                          ),
-                          Text('₹10')
-                        ],
-                      ),
-                      kHeight38,
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Order Total',
-                            style: TextStyle(
-                                fontSize: 17, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            '₹2010',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          )
-                        ],
-                      ),
-                      kHeight30,
-                      Divider(),
-                      kHeight30,
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Shipping Address',
-                            style: TextStyle(
-                                fontSize: 17, fontWeight: FontWeight.bold),
-                          ),
-                          TextButton(
-                              onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) =>
-                                        ChangeAddressScreen()));
-                              },
-                              child: Text(
-                                'Change',
-                                style: TextStyle(color: kGrey),
-                              ))
-                        ],
-                      ),
-                      kHeight30,
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Hareesh Haridas',
-                            style: TextStyle(fontSize: 17),
-                          ),
-                        ],
-                      ),
-                      kHeight30,
-                      Row(
-                        children: [
-                          Icon(Icons.call),
-                          Text(
-                            '8590170626',
-                            style: TextStyle(fontSize: 17),
-                          ),
-                        ],
-                      ),
-                      kHeight30,
-                      Row(
-                        children: [
-                          Icon(Icons.location_on_outlined),
-                          Expanded(
-                            child: Text(
-                              'Velamparambil House Eroor Tripunithura',
-                              style: TextStyle(fontSize: 17),
-                            ),
-                          ),
-                        ],
-                      ),
-                      kHeight10,
-                      MaterialButton(
-                        onPressed: () {
-                          // Your onPressed logic here
-                        },
-                        color: Colors.green,
-                        textColor: kWhite,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        height: 50,
-                        minWidth: 300,
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+              BlocBuilder<AddressBloc, AddressState>(
+                builder: (context, state) {
+                  if (state is LoadingAddressState) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is LoadedAddressState) {
+                    String? name;
+                    int? phoneNumber;
+                    String? otherDetails;
+                    List<GetAddressModel> address = state.address;
+                    for (int i = 0; i < address.length; i++) {
+                      if (selected == address[i].id) {
+                        name = address[i].name;
+                        phoneNumber = address[i].phoneNumber;
+                        otherDetails = address[i].cityName;
+                        print('Name : $name');
+                      }
+                    }
+                    return Container(
+                      height: 550,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: kGrey),
+                          borderRadius: BorderRadius.circular(13)),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 13),
+                        child: Column(
                           children: [
-                            kWidth30,
-                            Text(
-                              'Buy Now',
-                              style: TextStyle(color: kWhite, fontSize: 20),
+                            kHeight20,
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Subtotal',
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                                Text('₹2000')
+                              ],
+                            ),
+                            kHeight38,
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Shipping Charge',
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                                Text('₹10')
+                              ],
+                            ),
+                            kHeight38,
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Order Total',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  '₹2010',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20),
+                                )
+                              ],
+                            ),
+                            kHeight30,
+                            const Divider(),
+                            kHeight10,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'Payment Method',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                TextButton(
+                                    onPressed: () {},
+                                    child: const Text(
+                                      'Change',
+                                      style: TextStyle(color: kGrey),
+                                    ))
+                              ],
+                            ),
+                            Row(
+                              children: [Text('Cash on Delivery')],
+                            ),
+                            kHeight30,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'Shipping Address',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                TextButton(
+                                    onPressed: () async {
+                                      final newSelectedAddress = Navigator.of(
+                                              context)
+                                          .push<String>(MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const ChangeAddressScreen()));
+                                      if (newSelectedAddress != null) {
+                                        setState(() {
+                                          selected =
+                                              newSelectedAddress.toString();
+                                        });
+                                      }
+                                    },
+                                    child: const Text(
+                                      'Change',
+                                      style: TextStyle(color: kGrey),
+                                    ))
+                              ],
+                            ),
+                            kHeight30,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  selected != null && selected.isNotEmpty
+                                      ? name ?? ''
+                                      : address[0].name,
+                                  style: const TextStyle(fontSize: 17),
+                                ),
+                              ],
+                            ),
+                            kHeight30,
+                            Row(
+                              children: [
+                                const Icon(Icons.call),
+                                Text(
+                                  selected != null && selected.isNotEmpty
+                                      ? phoneNumber.toString()
+                                      : address[0].phoneNumber.toString(),
+                                  style: const TextStyle(fontSize: 17),
+                                ),
+                              ],
+                            ),
+                            kHeight30,
+                            Row(
+                              children: [
+                                const Icon(Icons.location_on_outlined),
+                                Expanded(
+                                  child: Text(
+                                    selected.isNotEmpty
+                                        ? otherDetails ?? ''
+                                        : '${address[0].cityName},${address[0].streetName}',
+                                    style: const TextStyle(fontSize: 17),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            kHeight10,
+                            MaterialButton(
+                              onPressed: () {
+                                // Your onPressed logic here
+                              },
+                              color: Colors.green,
+                              textColor: kWhite,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              height: 50,
+                              minWidth: 300,
+                              child: const Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  kWidth30,
+                                  Text(
+                                    'Buy Now',
+                                    style:
+                                        TextStyle(color: kWhite, fontSize: 20),
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         ),
                       ),
-                    ],
-                  ),
-                ),
+                    );
+                  } else if (state is ErrorAddressState) {
+                    return const Center(child: Text('Error Loading Addresses'));
+                  } else {
+                    return const Center(child: Text('unknown Error'));
+                  }
+                },
               )
             ],
           )),
